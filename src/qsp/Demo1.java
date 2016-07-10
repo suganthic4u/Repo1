9952a54fdfe9fffa0309ac44c983277dd0eaf109
suganthic4u <suganthic4u@gmail.com>
2016-07-10 15:03:21 +0530

    Update Demo1.java

--- src/qsp/Demo1.java
+++ src/qsp/Demo1.java
@@ -2,9 +2,9 @@ package qsp;
 
 public class Demo1 {
 	public static void main(String[] args) {
-		System.out.println("Hello Demo1");
-		System.out.println("Hello1 Demo1");
-		System.out.println("Hello2 Demo2");
+		System.out.println("Hello Happy Demo1");
+		System.out.println("Hello1 Happy Demo1");
+		System.out.println("Hello2 Happy Demo2");
 
 }
 }
