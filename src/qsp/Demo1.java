5e185cc26e436593259437bed6ac6f3fd9f10e55
Suganthi <suganthic4u@gmail.com>
2016-07-10 15:01:52 +0530

    demo1

---
+++ src/qsp/Demo1.java
@@ -0,0 +1,10 @@
+package qsp;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		System.out.println("Hello Demo1");
+		System.out.println("Hello1 Demo1");
+		System.out.println("Hello2 Demo2");
+
+}
+}
